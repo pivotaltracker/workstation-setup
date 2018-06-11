c99ff5e983e2142d79381ddcffb9165740d63cc6
Sumner Evans <jevans@pivotal.io>
2018-06-11 15:48:48 -0600

    Git config and aliases
    Signed-off-by: Eric Dattore <edattore@pivotal.io>

--- scripts/tracker/git.rb
+++ scripts/tracker/git.rb
@@ -8,6 +8,16 @@ class ConfigureGitTracker < TrackerConfigurationBase
   def run
     # Add Tracker .git-authors
     FileUtils.copy("#{repo_root}/files/tracker/.git-authors", "#{home}/.git-authors")
+
+    # Do the Tracker-specific Git configuration
+    set_git_config('pull.rebase', 'preserve')
+    set_git_config('push.default', 'current')
+    set_git_config('user.email', 'labs-tracker-team@pivotal.io')
+    set_git_config('user.name', 'Pivotal Tracker')
+  end
+
+  def set_git_config(key, value)
+    process("git config --global '#{key}' '#{value}'")
   end
 end
 
