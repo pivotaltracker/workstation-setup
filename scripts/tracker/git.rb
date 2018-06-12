e258b55e3c20ae788db57da6cfb9fea2f00682a3
Eric Dattore <edattore@pivotal.io>
2018-06-12 10:40:23 -0600

    comment out github repo cloning for now
    Signed-off-by: Sumner Evans <jevans@pivotal.io>

--- scripts/tracker/git.rb
+++ scripts/tracker/git.rb
@@ -15,41 +15,41 @@ class ConfigureGitTracker < TrackerConfigurationBase
     set_git_config('user.email', 'labs-tracker-team@pivotal.io')
     set_git_config('user.name', 'Pivotal Tracker')
 
-    tracker_repos = %w(
-      app_env
-      app_env_secret_nonprod
-      checkfiles
-      gitpflow
-      lib-tracker-proxy
-      tracker-android
-      tracker-docs
-      tracker-ios
-      tracker-knowledge-base
-      tracker-marketing
-      tracker-vcr
-    )
-    tracker_repos.each do |repo|
-      clone_git_repo('pivotaltracker', repo)
-    end
-
-    go_repos = %w(gogator gusher titan)
-    go_repos.each do |repo|
-      clone_git_repo('pivotaltracker', repo, path='~/go/src/github.com/pivotaltracker')
-    end
-
-    direnv_allow_repos = %w(tracker tracker-concourse)
-    direnv_allow_repos.each do |repo|
-      clone_git_repo('pivotaltracker', repo, direnv=true)
-    end
-
-    clone_git_repo('pivotaltracker', 'tracker', path='~/workspace/alt-tracker', direnv=true)
-
-    clone_git_repo(
-      'pivotaltracker',
-      'app_env_secret_nonprod',
-      path='~/.app_env_secret',
-      post_clone_command='ln -s ~/.app_env_secret/keys/npmrc ~/.npmrc'
-    )
+    # tracker_repos = %w(
+    #   app_env
+    #   app_env_secret_nonprod
+    #   checkfiles
+    #   gitpflow
+    #   lib-tracker-proxy
+    #   tracker-android
+    #   tracker-docs
+    #   tracker-ios
+    #   tracker-knowledge-base
+    #   tracker-marketing
+    #   tracker-vcr
+    # )
+    # tracker_repos.each do |repo|
+    #   clone_git_repo('pivotaltracker', repo)
+    # end
+    #
+    # go_repos = %w(gogator gusher titan)
+    # go_repos.each do |repo|
+    #   clone_git_repo('pivotaltracker', repo, path='~/go/src/github.com/pivotaltracker')
+    # end
+    #
+    # direnv_allow_repos = %w(tracker tracker-concourse)
+    # direnv_allow_repos.each do |repo|
+    #   clone_git_repo('pivotaltracker', repo, direnv=true)
+    # end
+    #
+    # clone_git_repo('pivotaltracker', 'tracker', path='~/workspace/alt-tracker', direnv=true)
+    #
+    # clone_git_repo(
+    #   'pivotaltracker',
+    #   'app_env_secret_nonprod',
+    #   path='~/.app_env_secret',
+    #   post_clone_command='ln -s ~/.app_env_secret/keys/npmrc ~/.npmrc'
+    # )
   end
 
   def set_git_config(key, value)
@@ -64,7 +64,7 @@ class ConfigureGitTracker < TrackerConfigurationBase
       end
     end
 
-    if post_clone_command != ''
+    unless post_clone_command.empty?
       FileUtils.cd(path) do
         process(post_clone_command)
       end
