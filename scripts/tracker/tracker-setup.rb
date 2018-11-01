a0364d27f64994837b1428ec88278f6eb67708ec
Nick Seemiller <nseemiller@pivotal.io>
2018-11-01 15:22:03 -0600

    [#161626664] comment failing nvm command

--- scripts/tracker/tracker-setup.rb
+++ scripts/tracker/tracker-setup.rb
@@ -30,7 +30,7 @@ class TrackerSetup < TrackerConfigurationBase
     end
 
     # Install Node
-    process_with_output('nvm install 10')
+    # process_with_output('nvm install 10')
 
     # Run direnv allow on all directories that require it.
     Dir.glob("#{home}/workspace/tracker/apps/tracker-web/**/.envrc").each do |envrc|
