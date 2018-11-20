0885ac0afe527dce70f1c116df3f0473aa205958
Chad Woolley <cwoolley@pivotal.io>
2018-11-19 22:59:48 -0700

    Revert "stuff to get RubyMine & IntelliJ auto configure"
    This reverts commit 5ec7a1518bc5576fe6242b87b65c968c52f5ee53.

--- scripts/tracker/rubymine.sh
+++
@@ -1,9 +0,0 @@
-echo
-# guard against pre-installed rubymine
-brew cask install rubymine --force
-
-source ${MY_DIR}/scripts/common/download-pivotal-ide-prefs.sh
-pushd ~/workspace/pivotal_ide_prefs/cli
-./bin/ide_prefs install --ide=intellij
-./bin/ide_prefs install --ide=rubymine
-popd
--- setup.sh
+++ setup.sh
@@ -53,7 +53,6 @@ source ${MY_DIR}/scripts/common/applications-common.sh
 
 # Install the Tracker desktop applications.
 ${MY_DIR}/scripts/tracker/applications-common.rb
-${MY_DIR}/scripts/tracker/rubymine.sh
 
 source ${MY_DIR}/scripts/common/unix.sh
 
@@ -101,7 +100,6 @@ ${MY_DIR}/scripts/tracker/tracker-setup.rb
 #    fi
 #done
 
-cp ${MY_DIR}/files/tracker/com.apple.dock.plist ~/Library/Preferences/
 ${MY_DIR}/scripts/tracker/finished.rb
 
 if (( SKIP_ANALYTICS == 0 )); then
